f2b1d181983be399b854081ff55990da8e9498e1
Phuong Quan <phuong.quan@ndm.ox.ac.uk>
2021-12-16 16:59:08 +0000

    Remove startup message on attach

--- R/zzz.R
+++ R/zzz.R
@@ -5,9 +5,3 @@ packageenvironment <- new.env(parent = emptyenv())
 #  rely on data.table functionality even though it is not mentioned in NAMESPACE file.
 #  This is to ensure the "[" function works even though it can't be prefixed with data.table::
 .datatable.aware = TRUE
-
-# TODO: move this main file?  Or maybe just drop.  Don't like how the message prints in red
-# display startup message to console when package is attached
-.onAttach <- function(libname, pkgname) {
-	packageStartupMessage("Automatic Change-point Detection for Electronic Health Records")
-}
